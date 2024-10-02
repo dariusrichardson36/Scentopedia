bc887022ab1e0dcd58400a97771e66cd1e34b14a
GitMonie <edyaltamirano21832@gmail.com>
2024-10-02 16:12:05 -0500

    Beginning of landing page dev.

--- react-enviroment/src/App.tsx
+++ react-enviroment/src/App.tsx
@@ -3,11 +3,12 @@
 import Navbar from './components/Navbar.tsx'; // Adjust path as needed
 import 'bootstrap';
 
+
 function App() {
   return (
-    <div>
+    <div className="bg-[url('public/parfums-de-marly-layton-featured-image-1024x640.png.jpeg')] bg-cover bg-local w-full h-screen">
       <Navbar /> {/* Use the Navbar component here */}
-      <h1>Welcome to Scentopedia</h1>
+      <h1 className="text-white text-center  text-3xl py-10">Welcome to Scentopedia</h1>
       {/* Your other components and content */}
     </div>
   );
