7e06177f8d14d8fb0d7cb4d0d8d55da538a03cc5
LittleBigBlarg <vance5r@hotmail.com>
2024-10-07 23:55:59 -0500

    Included routing to app.tsx

--- react-enviroment/src/pages/About.tsx
+++ react-enviroment/src/pages/About.tsx
@@ -1,11 +1,16 @@
 // src/pages/About.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const About: React.FC = () => {
+  useEffect(() => {
+    // This will log an error message in the console whenever the About page is rendered
+    console.error('Error: You have navigated to the About page!');
+  }, []); // The empty dependency array means this effect runs once when the component mounts
+
   return (
-    <div className="text-white text-center py-10">
-      <h2>About Scentopedia</h2>
-      <p>Learn more about our mission and the fragrances we recommend.</p>
+    <div className="text-black text-center py-10">
+      <h2 className="text-4xl mb-4" style={{ fontFamily: 'Bebas Neue, normal' }}>About Scentopedia</h2>
+      <p className="text-2xl">Learn more about our mission and the fragrances we recommend.</p>
     </div>
   );
 };
