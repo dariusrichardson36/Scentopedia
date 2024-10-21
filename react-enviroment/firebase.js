2bec7e1e65075dd6d0262fec0caf0c31e42a3d7f
Kacey Flores <flores.kacey@gmail.com>
2024-10-21 16:49:11 -0500

    adding firebase connection

---
+++ react-enviroment/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2gistUGA6Z3wT32Dt7oZF5BP4-sBQcR0",
+  authDomain: "scentopedia-ab22e.firebaseapp.com",
+  projectId: "scentopedia-ab22e",
+  storageBucket: "scentopedia-ab22e.appspot.com",
+  messagingSenderId: "295858152820",
+  appId: "1:295858152820:web:70c7f452941191264cf821"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
