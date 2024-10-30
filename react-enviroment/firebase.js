ddf9059afbd9e26be4529b424754dfdd10367380
Jose Barrera <jfbarrera2002@gmail.com>
2024-10-29 20:14:57 -0500

    Added firebase services.

--- react-enviroment/firebase.js
+++ react-enviroment/firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
+// src/lib/firebase.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+//Initialize Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
